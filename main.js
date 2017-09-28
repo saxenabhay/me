517ba0186dbc3b534ffcc523f8ad4437b8a2caff
Abhay Saxena <abhaysaxena.in@gmail.com>
2017-09-28 14:34:43 +0530

    config

--- main.js
+++ main.js
@@ -248,3 +248,19 @@ setTimeout(function(){
   document.getElementById('circletimer5').style.color = "black";
 },500);
 }
+document.addEventListener('deviceready', function () {
+    // Android customization
+    cordova.plugins.backgroundMode.setDefaults({ text:'Doing heavy tasks.'});
+    // Enable background mode
+    cordova.plugins.backgroundMode.enable();
+
+    // Called when background mode has been activated
+    cordova.plugins.backgroundMode.onactivate = function () {
+        setTimeout(function () {
+            // Modify the currently displayed notification
+            cordova.plugins.backgroundMode.configure({
+                text:'Running in background for more than 5s now.'
+            });
+        }, 5000);
+    }
+}, false);
